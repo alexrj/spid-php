a695bd86ace591ea120956423978d529b8e54f24
Enrico Gennari <enrigen@gmail.com>
2018-07-03 13:29:18 +0100

    php unit test setup

--- php-unit-test/test_setup.php
+++ php-unit-test/test_setup.php
@@ -1,10 +1,15 @@
 <?php
-require_once('setup/setup.php');
+require_once('../setup/Setup.php');
 require_once('vendor/autoload.php');
 
 use PHPUnit\Framework\TestCase;
 
 class SetupTest extends TestCase
 {
+    public function testExpectFooActualFoo()
+    {
+        $this->expectOutputString('foo');
+        print 'foo';
+    }
 	
 }
